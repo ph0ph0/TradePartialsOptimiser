177e25bb3086d52dfc18cedf8716eb231935e5bf
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-05-14 08:44:48 +0100

    finished rough structure, not calculating properly

--- PartialsCalculator.py
+++ PartialsCalculator.py
@@ -22,7 +22,7 @@ import numpy as np
 # This will generate a range of values for the net profit, each should be stored as a tuple, which has the first element as an array of partial percentages, the second as the NP
 
 def Caclulate_CumSum_Profit(Partials, AtR):
-    total_shares = 100
+    total_shares = 1000
     remaining_shares = total_shares
     total_profit = 0
     profit_array = []
@@ -44,11 +44,14 @@ def Total_Profit(ProbabilityAtR, CumSum):
     no_of_trades = 1000
     no_of_trades_AtEachR = []
     for probability in ProbabilityAtR:
+        # Using the probability, calculate the number of trades that will go through each R.
         no_of_trades_AtEachR.append(no_of_trades * (probability / 100))
 
-    # Calculate the number of winners at each R
+    # Calculate the number of trades that only hit each R, then turn around and go to BE.
     no_of_winners_AtR = []
     for idx, no_of_trades in enumerate(no_of_trades_AtEachR):
+        # Out of 1000 trades, 70% are 2R, 60% are 3R, 30% are 4R, and 5% are 6R. This means that of the 700 trades that reach 2R, 600 will go on to be 3R trades. That means that 
+        # only 100 trades will be purely 2R trades (2R, then back to BE). Of the 600, 300 will go on to be 4R, leaving 300 and 3R. Finally, 50 will be full R trades, so 250 will be left at 4R
         # The last element for number of trades doesn't need updating.
         # If the probabilities were 70, 60, 10, and we had 1000 trades, then the last will always be equal to probability * no_of_trades (= 100)
         if idx == len(no_of_trades_AtEachR) - 1:
@@ -56,8 +59,9 @@ def Total_Profit(ProbabilityAtR, CumSum):
             # print(no_of_winners_AtR)
             break
         no_of_winners_AtR.append(no_of_trades - no_of_trades_AtEachR[idx + 1])
-    
-    # Now simply multiply the no_of_winners_AtR by the realised profit at each R
+
+    # Now simply multiply the no_of_winners_AtR by the realised profit at each R. This will tell you how much money each trade that got to that R and turned around to BE
+    # made. For example, if a trade that gets to 2R and drops 30%, then hits BE will make 0.6R, then 1000 of these will make 600R. 
     x = []
     for idx, t in enumerate(CumSum):
         # print(t)
