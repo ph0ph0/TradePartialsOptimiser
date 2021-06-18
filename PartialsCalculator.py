cb5a077a42e8edf638b639cae13f32eed0f68bec
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-06-18 14:38:37 +0100

    updated a few lines of the introductory message

--- PartialsCalculator.py
+++ PartialsCalculator.py
@@ -1,19 +1,26 @@
 from functools import reduce
 import numpy as np
 
+# 70% of trades get to 2R
+# 60% get to 3R
+# 10% get to 4R
+
 # This assumes SL to BE after first partial.
-# We take 30% of our position at 2R. This is 60 dolls out of 100. However, out of 1000 trades, only 70% get to 2R. That means 700 * 60 = 42,000 dolls realised, 300 * 100 = 30,000 dolls lost, net 12,000 profit.
+# We have 100 shares and $1 risk on the position. This means that the share/$ risk is $100, so 2R = $200 etc.
+# We take 30% of our position at 2R. This is 60 dolls out of 200, leaving $140 on the table and 70 shares.. However, out of 1000 trades, only 70% get to 2R. That means 700 * 60 = 42,000 dolls realised, 300 * 100 = 30,000 dolls lost, net 12,000 profit 
+# (in other words, 300 trades out of the 1000 don't make it to 2R and go back to -1R loss).
 # Of those 700 that get to 2R, only 14.28% fail to get to 3R (700 - 600) / 700. The profit of a 60% partial at 3R is 126 dolls. If the trade gets this far, then you will have 186 dolls realised.
 # This means that out of 700 profitable trades, 14.28% (100 trades) make only 60 dolls, and 85.72% (600 trades) make 126 dolls. 100 * 60 = 6000, 600 * 126 = 75,600, sum = 81,600. Again, you would lose 30,000 from those that didnt make 2R, and so net is 51,600 dolls. If we were to stop here, then that would be it.
 # If we have more than two partials, we need to consider all of the partial probabilities together. So we have 700/1000 make it to 2R, 600/1000 make it to 3R, and 100/1000 make it to 4R. So 85.72% of trades that made it to 2R make it to 3R, and 16.67% of trades that made it 3R will make it to 4R. 
 # So previously, we had 600 trades that got to 3R. 16.67% of these (100/600), 100 trades, will make it to 4R. 
-# so now: 100 trades make 60 dolls, 500 trades make 186 dolls, and 100 trades make 298 dolls = 98,800 gross profit, 68,800 net profit 
+# so now: 100 trades make 60 dolls, 500 trades make 186 dolls, and 100 trades make 298 dolls, 300 trades lose 100 dolls = 128,800 gross profit, 98,800 net profit 
 
 # so it goes: 
 # 6000
 # 93000
 # 29800
-# total = 128,800
+# gross = 128,800
+# net = 98,800
 
 # So what we need to do is firstly calculate the profit according to where you are taking the partials. 
 # Then we need to calculate the profit according to probability, as described above. 
